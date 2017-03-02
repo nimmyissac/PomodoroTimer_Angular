c6bbf7e4e967021ea9c998c5e79101e04d53512b
Nimmy Issac <nimmyissac2014@gmail.com>
2017-03-02 10:31:40 -0800

    color changes for breaks and sessions and also took care of display for single digits and zero values

--- pomodoro.js
+++ pomodoro.js
@@ -11,11 +11,23 @@ function displayTime($scope) {
   var runTimer;
   $scope.isSession = true;
     
+  var fillColor = function(isSession) {
+    if(!isSession) {
+      (document.getElementsByClassName("header1")[0]).style.backgroundColor = "red";
+      (document.getElementsByClassName("header2")[0]).style.backgroundColor = "#A1A1A1";
+    // (".header1").style.backgroundColor = "green";
+    } else {
+      (document.getElementsByClassName("header2")[0]).style.backgroundColor = "green";
+      (document.getElementsByClassName("header1")[0]).style.backgroundColor = "#A1A1A1";
+    }
+  }
   $scope.divOnClick = function() {
     $scope.startTimer = !($scope.startTimer);
     if($scope.startTimer) {
       console.log("$scope.startTimer ", $scope.startTimer);
       runTimer = setInterval(function() {
+                  fillColor($scope.isSession);
+                     
                   if($scope.seconds === 0) {
                     if($scope.minutes ===0) {
                      /*----after a session is over Break Period starts------*/
@@ -30,7 +42,9 @@ function displayTime($scope) {
                   } else {
                     $scope.seconds = ($scope.seconds - 1) ; 
                   }
-                 $scope.displaySeconds = ":" + $scope.seconds;
+                 var secondsLength = $scope.seconds.toString().length;
+                 $scope.displaySeconds = (secondsLength > 1)?(":" + $scope.seconds):(secondsLength === 1)?(":0" + $scope.seconds) : (":00" + $scope.seconds)
+                  
                  $scope.$apply();
                },1000);
       } else {
@@ -58,4 +72,5 @@ function displayTime($scope) {
       }
 }
 }
+
 
